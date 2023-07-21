be199dcf77aa7403812d2386635616e5e4913c09
isevou__ <kbry12@naver.com>
2023-07-21 23:56:06 +0900

    test : create_user() test (User class에서 GenerationType.IDENTITY로 하면 Error떠서 임시로 없애줌)

---
+++ src/test/java/myplace/core/user/repository/UserRepositoryTest.java
@@ -0,0 +1,45 @@
+package myplace.core.user.repository;
+
+import myplace.core.user.dao.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class UserRepositoryTest {
+    @Autowired
+    UserRepository userRepository;
+
+    @AfterEach
+    public void cleanup(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("유저 생성 확인")
+    public void create_user() {
+        Long datetime = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(datetime);
+        userRepository.save(User.builder()
+                .id(1L)
+                .username("dkinda")
+                .name("daniel")
+                .email("kbry12@naver.com")
+                .password("1234")
+                .createdAt(timestamp)
+                .build());
+
+        List<User> userList = userRepository.findAll();
+
+        User user = userList.get(0);
+        assertThat(user.getName()).isEqualTo("daniel");
+        assertThat(user.getEmail()).isEqualTo("kbry12@naver.com");
+    }
+}
