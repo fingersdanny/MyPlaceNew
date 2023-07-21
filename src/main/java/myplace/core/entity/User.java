91e846a38372aee3c0342ebdcb59af5e8a3eef8e
isevou__ <kbry12@naver.com>
2023-07-21 21:56:46 +0900

    add : User class annotation 수정

--- src/main/java/myplace/core/entity/User.java
+++ src/main/java/myplace/core/entity/User.java
@@ -1,25 +1,33 @@
 package myplace.core.entity;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
+@Getter
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     @Column(unique = true)
     private String username;
+
     private String password;
+
     @Column(unique = true)
     private String email;
+
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
