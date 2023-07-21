26521536ff65645a058fa5f04444fe9fbcb5a8f9
isevou__ <kbry12@naver.com>
2023-07-22 00:23:20 +0900

    add : UserTimeEntity 상속 받도록 수정

--- src/main/java/myplace/core/user/dao/User.java
+++ src/main/java/myplace/core/user/dao/User.java
@@ -11,7 +11,7 @@ import java.util.Date;
 @Getter
 @Builder
 @Table(name = "users")
-public class User {
+public class User extends UserTimeEntity {
     @Id
     @GeneratedValue
     //@GeneratedValue(strategy=GenerationType.IDENTITY)
