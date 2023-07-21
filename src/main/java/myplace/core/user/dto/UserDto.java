f0332433cbc857247d6874bcbf092a125fc45071
isevou__ <kbry12@naver.com>
2023-07-22 00:09:27 +0900

    add : UserDto 추가

---
+++ src/main/java/myplace/core/user/dto/UserDto.java
@@ -0,0 +1,41 @@
+package myplace.core.user.dto;
+
+import lombok.*;
+import myplace.core.user.dao.User;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    private String email;
+    private Date createdAt;
+
+    @Builder
+    public UserDto(Long id, String name, String username, String password, String email, Date createdAt) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+
+    public User toEntity() {
+        User user = User.builder()
+                .id(id)
+                .name(name)
+                .username(username)
+                .password(password)
+                .email(email)
+                .createdAt(createdAt)
+                .build();
+        return user;
+    }
+}
