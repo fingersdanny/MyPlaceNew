b66b6a2c0591abdec489bb744f4530395986b30c
isevou__ <kbry12@naver.com>
2023-07-23 08:28:55 +0900

    fix : @PropertySource 추가 (application.properties 숨김 처리)

--- src/main/java/myplace/core/CoreApplication.java
+++ src/main/java/myplace/core/CoreApplication.java
@@ -3,8 +3,10 @@ package myplace.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@PropertySource("file:C:/Users/finge/IdeaProjects/core/MyPlaceSecurity/application.properties")
 @EnableJpaAuditing
 @SpringBootApplication
 public class CoreApplication {
