d776de651d5eceba46b4cf01fb7796587078f85b
isevou__ <kbry12@naver.com>
2023-07-28 12:24:18 +0900

    fix : submodule 재설정

--- src/main/java/myplace/core/CoreApplication.java
+++ src/main/java/myplace/core/CoreApplication.java
@@ -2,11 +2,12 @@ package myplace.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@PropertySource(value = "file:MyPlaceAuth/application.properties")
 public class CoreApplication {
 
     public static void main(String[] args) {
