1622ce5d8802c38469bad10054015a54a39fe8e3
isevou__ <kbry12@naver.com>
2023-08-02 11:58:42 +0900

    fix : userDto @NotBlank annotation 수정

--- src/main/java/myplace/core/user/dto/UserDto.java
+++ src/main/java/myplace/core/user/dto/UserDto.java
@@ -3,9 +3,7 @@ package myplace.core.user.dto;
 import lombok.*;
 import myplace.core.user.domain.User;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @Getter
 @Setter
@@ -13,19 +11,19 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 public class UserDto {
     private Long id;
-    @NotEmpty(message = "이름은 필수항목입니다.")
+    @NotBlank(message = "이름은 필수항목입니다.")
     private String name;
 
     @Size(min = 4, message = "닉네임은 4글자 이상이어야 합니다")
-    @NotEmpty(message = "닉네임은 필수항목입니다.")
+    @NotBlank(message = "닉네임은 필수항목입니다.")
     private String username;
 
-    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    @NotBlank(message = "비밀번호는 필수항목입니다.")
     @Size(min = 8, max = 20, message = "비밀번호는 8자리 이상이어야 합니다.")
     private String password;
 
     @Email
-    @NotEmpty(message = "이메일은 필수항목입니다.")
+    @NotBlank(message = "이메일은 필수항목입니다.")
     private String email;
 
     @Builder
