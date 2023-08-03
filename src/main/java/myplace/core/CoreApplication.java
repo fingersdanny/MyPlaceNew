b49e16d1a683928fb6827dcc387aaef9f73dbd4f
isevou__ <kbry12@naver.com>
2023-08-03 13:16:05 +0900

    fix : userList 디자인 추가

--- src/main/java/myplace/core/CoreApplication.java
+++ src/main/java/myplace/core/CoreApplication.java
@@ -2,11 +2,14 @@ package myplace.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @PropertySource("file:C:/Users/finge/IdeaProjects/core/MyPlaceAuth/application-main.yml")
+@PropertySource("file:C:/Users/finge/IdeaProjects/core/MyPlaceAuth/application-API-key.properties")
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class CoreApplication {
 
---
+++ src/main/java/myplace/core/commons/client/KakaoFeignClient.java
@@ -0,0 +1,22 @@
+package myplace.core.commons.client;
+
+import myplace.core.place.domain.Place;
+import myplace.core.place.dto.PlaceSaveDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "kakaoClient", url = "https://dapi.kakao.com")
+public interface KakaoFeignClient {
+
+    //@PathVariable을 통해 가변 경로 설정 가능
+    @GetMapping("/v2/local/search/category.json")
+    List<PlaceSaveDto> findPlacesByQuery(@RequestHeader(value = "Authorization") String authorizationHeader,
+                                         @RequestParam(value = "category_group_code") String categoryGroupCode);
+
+}
