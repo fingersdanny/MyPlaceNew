8cd7f91a45c61fc6e473f51e345e0c98e7f86d3c
isevou__ <kbry12@naver.com>
2023-08-04 03:25:32 +0900

    fix : Place domain 및 PlaceSaveDto를 api 응답 형식에 맞도록 수정

--- src/main/java/myplace/core/place/domain/Place.java
+++ src/main/java/myplace/core/place/domain/Place.java
@@ -11,37 +11,44 @@ import javax.persistence.*;
 @Table(name = "places")
 public class Place {
     @Id
-    @GeneratedValue
     @Column(name = "place_id")
     private Long id;
 
     private String placeName;
 
+    private String categoryName;
+
+    private String categoryGroupCode;
+
+    private String categoryGroupName;
+
+    private String phone;
+
     private String addressName;
 
     private String roadAddressName;
 
+    private String x;
+
+    private String y;
+
     private String placeUrl;
 
-    private String phone;
-
-    private String categoryGroupName;
-
-    private double x;
-
-    private double y;
+    private String distance;
 
     @Builder
-
-    public Place(Long id, String placeName, String addressName, String roadAddressName, String placeUrl, String phone, String categoryGroupName, double x, double y) {
+    public Place(Long id, String placeName, String categoryName, String categoryGroupCode, String categoryGroupName, String phone, String addressName, String roadAddressName, String x, String y, String placeUrl, String distance) {
         this.id = id;
         this.placeName = placeName;
+        this.categoryName = categoryName;
+        this.categoryGroupCode = categoryGroupCode;
+        this.categoryGroupName = categoryGroupName;
+        this.phone = phone;
         this.addressName = addressName;
         this.roadAddressName = roadAddressName;
-        this.placeUrl = placeUrl;
-        this.phone = phone;
-        this.categoryGroupName = categoryGroupName;
         this.x = x;
         this.y = y;
+        this.placeUrl = placeUrl;
+        this.distance = distance;
     }
 }
--- src/main/java/myplace/core/place/dto/PlaceSaveDto.java
+++ src/main/java/myplace/core/place/dto/PlaceSaveDto.java
@@ -1,5 +1,6 @@
 package myplace.core.place.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import myplace.core.place.domain.Place;
 
@@ -8,47 +9,62 @@ import myplace.core.place.domain.Place;
 @Setter
 @ToString
 public class PlaceSaveDto {
-
     private Long id;
 
     private String placeName;
 
-    private String addressName;
-    private String roadAddressName;
-    private String placeUrl;
+    private String categoryName;
 
-    private String phone;
+    private String categoryGroupCode;
 
     private String categoryGroupName;
 
-    private double x;
+    private String phone;
 
-    private double y;
+    private String addressName;
 
-    @Builder
-    public PlaceSaveDto(Long id, String placeName, String addressName, String roadAddressName, String placeUrl, String phone, String categoryGroupName, double x, double y) {
+    private String roadAddressName;
+
+    private String x;
+
+    private String y;
+
+    private String placeUrl;
+
+    private String distance;
+
+    public PlaceSaveDto(Long id, String placeName, String categoryName, String categoryGroupCode, String categoryGroupName, String phone, String addressName, String roadAddressName, String x, String y, String placeUrl, String distance) {
         this.id = id;
         this.placeName = placeName;
+        this.categoryName = categoryName;
+        this.categoryGroupCode = categoryGroupCode;
+        this.categoryGroupName = categoryGroupName;
+        this.phone = phone;
         this.addressName = addressName;
         this.roadAddressName = roadAddressName;
-        this.placeUrl = placeUrl;
-        this.phone = phone;
-        this.categoryGroupName = categoryGroupName;
         this.x = x;
         this.y = y;
+        this.placeUrl = placeUrl;
+        this.distance = distance;
     }
+
+    @Builder
+
 
     public Place toEntity() {
         Place place = Place.builder()
                 .id(id)
                 .placeName(placeName)
+                .categoryName(categoryName)
+                .categoryGroupCode(categoryGroupCode)
+                .categoryGroupName(categoryGroupName)
+                .phone(phone)
                 .addressName(addressName)
                 .roadAddressName(roadAddressName)
-                .placeUrl(placeUrl)
-                .phone(phone)
-                .categoryGroupName(categoryGroupName)
                 .x(x)
                 .y(y)
+                .placeUrl(placeUrl)
+                .distance(distance)
                 .build();
 
         return place;
